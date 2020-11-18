6ac2502bbc37f56d06130db757ad85723c2b4b43
David Romoff <DaveTheMovie@gmail.com>
2020-11-17 19:31:09 -0500

    Update test_GetBondPrice.py

--- test_GetBondPrice.py
+++ test_GetBondPrice.py
@@ -2,6 +2,6 @@
 
 import GetBondPrice
 
-def test_getBondPrice(y, face, couponRate, m,  ppy):
+def test_getBondPrice():
     assert round(GetBondPrice.getBondPrice(.03, 2000000, .04, 10,  1)) == 2170604
     assert round(GetBondPrice.getBondPrice(.03, 2000000, .04, 10,  2)) == 2171686
